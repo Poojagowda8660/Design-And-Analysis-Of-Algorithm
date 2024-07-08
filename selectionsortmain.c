8fe50dc6c3ac58457bed2d6643a4586b40d157b3
Pooja <158460476+Poojagowda8660@users.noreply.github.com>
2024-07-08 18:05:38 +0530

    Add files via upload

---
+++ selectionsortmain.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define 100
+int count=0;
+void selectionsort(int A[size],int n)
+{
+    int i,j,temp,min;
+    for(i=0;i<=n-2;i++)
+    {
+        min=i;
+        for(j=i+1;j<=n-1;j++)
+        {
+            count++;
+            if(A[j]<A[min])
+                min=j;
+        }
+        temp=A[i];
+        A[i]=A[min];
+        A[min]=temp;
+    }
+}
+
+int main()
+{
+    int i,A[size],n;
+    printf("\nRead array size:");
+    scanf("%d",&n);
+    printf("\nRead array elements\n");
+    for(i=0;i<=n-1;i++)
+        scanf("%d",&A[i]);
+    selectionsort(A,n);
+    printf("\nSorted elements are\n");
+    for(i=0;i<=n-1;i++)
+        printf("%d\t",A[i]);
+    printf("\nCount=%d",count);
+    return 0;
+}
