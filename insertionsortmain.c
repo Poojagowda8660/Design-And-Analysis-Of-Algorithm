7b03de6ca3e38339f5247ad23240be0c9f13e917
Pooja <158460476+Poojagowda8660@users.noreply.github.com>
2024-07-09 09:47:30 +0530

    Add files via upload

---
+++ insertionsortmain.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define size 100
+int count=0;
+void selectionsort(int A[size],int n)
+{
+    int i,j,temp,item;
+    for(i=0;i<=n-1;i++)
+    {
+        item=A[i];
+        j=i-1;
+      while(j>=0 && A[j]>item)
+      {
+          A[j+1]=A[j];
+          j=j-1;
+          A[j+1]=item;
+      }
+    }
+}
+int main()
+{
+int i,A[size],n;
+printf("\nRead array size:");
+scanf("%d",&n);
+printf("\nRead array elements\n");
+for(i=0;i<=n-1;i++)
+    scanf("%d",&A[i]);
+selectionsort(A,n);
+printf("\nSorted elements are\n");
+for(i=0;i<=n-1;i++)
+    printf("%d\t",A[i]);
+printf("\nCount=%d",count);
+return 0;
+}
